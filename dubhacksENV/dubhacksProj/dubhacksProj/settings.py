619bba40b4e02db294fa75ed40a419737bfc1edd
GabrielDSM <gabriel.menescal@hotmail.com>
2016-10-16 03:04:31 -0700

    Sad Life
    Indentation fixes

--- dubhacksENV/dubhacksProj/dubhacksProj/settings.py
+++ dubhacksENV/dubhacksProj/dubhacksProj/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 -STATIC_URL = '/static/'
- -STATIC_ROOT = BASE_DIR + '/static/'
-  
- -STATICFILES_FINDERS = (
- -    'django.contrib.staticfiles.finders.FileSystemFinder',
- -    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
- -)
- 
+-STATIC_ROOT = BASE_DIR + '/static/'
+
+-STATICFILES_FINDERS = (
+-    'django.contrib.staticfiles.finders.FileSystemFinder',
+-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+-)
+
 STATIC_URL = '/static/'
