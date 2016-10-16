27ed1e58d6099183cc506d5e6f5e186d352df846
Vincent M. van der Meulen <meulen@uw.edu>
2016-10-16 06:35:31 -0700

    Almost time for breakfast

--- dubhacksENV/dubhacksProj/dubhacksProj/views.py
+++ dubhacksENV/dubhacksProj/dubhacksProj/views.py
@@ -6,13 +6,13 @@ def women(request):
     return render(request, 'women.html', {'page_name': 'Women'})
 
 def hcde(request):
-    return render(request, 'major.html', {'page_name': 'Human Centered Design and Engineering'})
+    return render(request, 'major.html', {'page_name': 'Human Centered Design & Engineering'})
 
 def info(request):
     return render(request, 'major.html', {'page_name': 'Informatics'})
 
 def cse(request):
-    return render(request, 'major.html', {'page_name': 'Computer Science and Engineering'})
+    return render(request, 'major.html', {'page_name': 'Computer Science & Engineering'})
 
 def about(request):
     return render(request, 'major.html', {'page_name': 'About'})
