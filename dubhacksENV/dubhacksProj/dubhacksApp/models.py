1a518bbb3d4776c4017d5c38bf32d3672892a9c8
GabrielDSM <gabriel.menescal@hotmail.com>
2016-10-15 21:28:30 -0700

    SQLite and person class
    Created a sqlite person class with the appropriate fields (-minus img
    field)

--- dubhacksENV/dubhacksProj/dubhacksApp/models.py
+++ dubhacksENV/dubhacksProj/dubhacksApp/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+	name = models.CharField(max_length = 45)
+	major = models.CharField(max_length = 55)
+	response1 = models.TextField()
+	response2 = models.TextField()
+	response3 = models.TextField()
+	response4 = models.TextField()
+	response5 = models.TextField()
