84760f186d3b99cc291214e78ff79ab48288d301
GabrielDSM <gabriel.menescal@hotmail.com>
2016-10-15 22:37:58 -0700

    Updated models.py
    SQLite classes

--- dubhacksENV/dubhacksProj/dubhacksApp/models.py
+++ dubhacksENV/dubhacksProj/dubhacksApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
+class Woman(models.Model):
 	name = models.CharField(max_length = 45)
 	major = models.CharField(max_length = 55)
 	response1 = models.TextField()
@@ -12,3 +12,13 @@ class Person(models.Model):
 	response3 = models.TextField()
 	response4 = models.TextField()
 	response5 = models.TextField()
+	# Add field for image, look into BLOB
+
+
+class Major(models.Model):
+	title = models.CharField(max_length = 55)
+	description = models.TextField()
+	link = CharField(max_length = 34)
+
+
+		
