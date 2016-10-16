ecbce61cfa71d19fe15f763411653a5be46808b7
GabrielDSM <gabriel.menescal@hotmail.com>
2016-10-16 03:39:03 -0700

    Renamed models

--- dubhacksENV/dubhacksProj/dubhacksApp/admin.py
+++ dubhacksENV/dubhacksProj/dubhacksApp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Woman, Major
+from .models import PersonEntry, Major
 
-admin.site.register(Woman)
+admin.site.register(PersonEntry)
 admin.site.register(Major)
--- dubhacksENV/dubhacksProj/dubhacksApp/models.py
+++ dubhacksENV/dubhacksProj/dubhacksApp/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Woman(models.Model):
+class PersonEntry(models.Model):
 	name = models.CharField(max_length=45)
 	major = models.CharField(max_length=55)
 	response1 = models.TextField()
